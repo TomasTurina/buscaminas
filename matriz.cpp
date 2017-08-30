51c367bbedae0605a6a1faaf2cbc9c4f48aff17c
Alejo Sagues <alejosagues@gmail.com>
2017-08-30 11:31:06 -0300

    funcion tablero

--- matriz.cpp
+++ matriz.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-int matriz[15][15] = {0};
+int tablero[15][15] = {0};
+void ftablero();
 
 int main(){
-	matriz[1][1]=2;
+	int bombas;
+	while(true){
+	std::cout<<"Ingrese la cantidad de bombas, entre 10 y 80: ";
+	std::cin>>bombas;
+	if (bombas<10 || bombas>80){
+		std::cout<<"Solo numeros entre 10 y 80\n";
+	}
+	else{
+	tablero[1][1]=2;
+	ftablero();
+	break;
+	}
+	}
+	return 0;
+}
+
+void ftablero(){
 	int i, j;
 	for(j=0;j<15;j++){          
 		
 		for(i=0;i<15;i++){
-			std::cout<<matriz[j][i];
+			std::cout<<tablero[j][i];
 			std::cout<<" ";	
 		}
 		std::cout<<"\n";
 	}
-	return 0;
+	return ;
 }
