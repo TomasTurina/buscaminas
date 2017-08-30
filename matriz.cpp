ac50df0c4aab0051746164bb77fdeeccdbabce7e
Alejo Sagues <alejosagues@gmail.com>
2017-08-30 12:57:27 -0300

    posicionamiento bombas y aleatoriedad

--- matriz.cpp
+++ matriz.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 int tablero[15][15] = {0};
 void ftablero();
+void frand(int);
 
 int main(){
+	srand(time(0));
 	int bombas;
 	while(true){
 	std::cout<<"Ingrese la cantidad de bombas, entre 10 y 80: ";
@@ -11,7 +15,19 @@ int main(){
 		std::cout<<"Solo numeros entre 10 y 80\n";
 	}
 	else{
-	tablero[1][1]=2;
+	//frand(bombas);
+	//std::cout<<"\n";
+	int rand1, rand2, i;
+	for(i = bombas;i>0;i--){
+		rand1 = rand() % 15;
+		rand2 = rand() % 15;
+		if(tablero[rand1][rand2]==7){
+			i++;
+		}
+		else{
+			tablero[rand1][rand2]=7;
+		}
+	}
 	ftablero();
 	break;
 	}
@@ -31,3 +47,13 @@ void ftablero(){
 	}
 	return ;
 }
+
+void frand(int bombas){
+	int num1, num2;
+	num1=rand() % bombas + 1;
+	std::cout<<num1<<"\n";
+	num2=rand() % bombas + 1;
+	std::cout<<num2;
+	return;
+}
+
